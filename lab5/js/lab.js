10e5778cc9e766938b63b5a19bbcd3cc099ba50f
shaochn <schen370@ucsc.edu>
2022-04-19 14:11:46 -0700

    lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author: Shao Chen and Laiy Joshi
+ * Created: 04.19.2022
+ * License: Public Domain
+ **/
+
+// Defining Variables //
+    var make = "Toyota";
+    var model = "Prius";
+    var color = "Purple";
+    var year = 1995;
+
+  // calculate //
+    age = 2022 - year;
+
+  // output //
+    document.writeln("Make: " + make + "<br>");
+    document.writeln("Model: " + model + "<br>");
+    document.writeln("Color: " + color + "<br>");
+    document.writeln("Year: " + year + "<br>");
+    document.writeln("Age: " + age + "<br>");

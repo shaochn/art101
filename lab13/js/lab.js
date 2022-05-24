be9af3ad67548a2c678c50f5878a7061311497c7
shaochn <schen370@ucsc.edu>
2022-05-24 00:59:55 -0700

    lab14

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,38 +7,38 @@
  function FizzBuzz(){
    // Loop through numbers 1 to 200, listing them as you go
     var oneLongString = "";
-    for (var i=1; i<201; i++) {
+    for (var i=1; i<201; i++) { i = 3
 
       // If the number is a multiple of 3 and 5, if should print "FizzBuzz!"
-        if (i % 15 == 0) {
+        if (i % 15 == 0) {  i = 3
           console.log("FizzBuzz!");
           oneLongString += i + "FizzBuzz!<br>";
         }
 
       // If the number is a multiple of 7 and 5, it should print ""
-        if (i % 35 == 0) {
+        if (i % 35 == 0) {  i = 3
           console.log("BoomBuzz!");
           oneLongString += i + "BoomBuzz!<br>";
         }
 
           // If the number is a multiple of 7 and 3, it should print "BoomFizz!"
-        if (i % 21 == 0) {
+        if (i % 21 == 0) {  i = 3
           console.log("BoomFizz!");
           oneLongString += i + "BoomFizz!<br>";
         }
 
       // If the number is a multiple of 7, it should print "Boom!"
-        else if (i % 7 == 0) {
+        else if (i % 7 == 0) {  i = 3
           console.log("Boom!");
           oneLongString += i + "Boom!<br>";
         }
         // If the number is a multiple of 5, it should print "Buzz!"
-        else if (i % 5 == 0) {
+        else if (i % 5 == 0) {  i = 3
           console.log("Buzz!");
           oneLongString += i + "Buzz!<br>";
         }
         // If the number is a multiple of 3, if should print "Fizz!"
-        else if (i % 3 == 0) {
+        else if (i % 3 == 0) {  i = 3
           console.log("Fizz!");
           oneLongString += i + "Fizz!<br>";
         }
